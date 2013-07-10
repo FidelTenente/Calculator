a720fd8c0dcf60328b1d08a8bcae6490b35918ac
FidelTenente <tenente@myttk.ru>
2013-07-10 14:36:06 +0700

    Тесты Exp,Odin,Tan

---
+++ CalculatorTest/ExpTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+namespace CalculatorTest
+{
+    /// <summary>
+    /// Сводное описание для ExpTest
+    /// </summary>
+    [TestClass]
+    public class ExpTest
+    {
+                #region Дополнительные атрибуты тестирования
+        //
+        // При написании тестов можно использовать следующие дополнительные атрибуты:
+        //
+        // ClassInitialize используется для выполнения кода до запуска первого теста в классе
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // ClassCleanup используется для выполнения кода после завершения работы всех тестов в классе
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // TestInitialize используется для выполнения кода перед запуском каждого теста 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // TestCleanup используется для выполнения кода после завершения каждого теста
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestExpWith10()
+        {
+            Exp calc = new Exp();
+            double res = calc.singleCalculation(10);
+            Assert.AreEqual(22026.465, res, 0.001, "Error exp with 10");
+            
+        }
+    }
+}
