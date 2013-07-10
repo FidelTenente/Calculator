b16acc87d0d66adae2bbad5c4d1eedd937a18e2c
19-alenka <elena-ka.1994@mail.ru>
2013-07-10 14:10:08 +0700

    добавлена функция "Exp"

---
+++ Calculator/Exp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public class Exp : singleOperation
+    {
+        public override double singleCalculation(double a)
+        {
+            return Math.Exp(a);
+
+        }
+    }
+}
+
